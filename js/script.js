46a3ab55189ae67501ddd025889df001e6d67b33
Erick Firmo <erickfirmo1996@gmail.com>
2022-09-29 23:49:44 -0300

    Update mobile settings

--- js/script.js
+++ js/script.js
@@ -1,8 +1,31 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
-const pipeVelocity = (1.5 / 2000) * window.innerWidth;
-pipe.style.animationDuration = `${pipeVelocity}s`;
+//pipe.style.animationDuration = `${0.0015 * window.innerWidth}s`;
+
+var pipeDistanceLeft = 73;
+var pipeDistanceBottom = 40;
+var marioWidth = 40;
+var marioLeft = 30;
+
+const loadGame = () => {
+    if(window.innerWidth < 767) {
+        // Mobile
+        pipeDistanceLeft = 73;
+        pipeDistanceBottom = 40;
+        marioWidth = 40;
+        marioLeft = 30;
+    } else {
+        // Desktop
+        pipeDistanceLeft = 120;
+        pipeDistanceBottom = 75;
+        marioWidth = 80;
+        marioLeft = 38;
+    }
+}
+
+window.onload = loadGame;
+window.onresize = loadGame;
 
 const jump = () => {
     mario.classList.add('jump');
@@ -18,15 +41,15 @@ const loop = setInterval(() => {
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
     const cloudsPosition = +window.getComputedStyle(clouds).right.replace('px', '');
 
-    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    if (pipePosition <= pipeDistanceLeft && pipePosition > 0 && marioPosition < pipeDistanceBottom) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
         mario.src = './images/game-over.png'
-        mario.style.width = '75px';
-        mario.style.marginLeft = '50px'
+        mario.style.width = `${marioWidth}px`;
+        mario.style.marginLeft = `${marioLeft}px`;
 
         clouds.style.animation = 'none';
         clouds.style.right = `${cloudsPosition}px`;
